c27d03fbc416e810b37f5acfe3e045667d066f64
Sean McEligot <sean.mceligot@gmail.com>
2021-05-23 22:25:57 -0400

    convert to seahorse

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@
 extern crate log;
 extern crate dotenv;
 extern crate env_logger;
-#[macro_use]
-extern crate clap;
+extern crate seahorse;
 //#[macro_use]
 extern crate config;
 extern crate dirs;
@@ -11,91 +10,12 @@ extern crate serde_derive;
 extern crate toml;
 
 use ansi_term::Colour::{Green, Red, Yellow};
-use clap::{App, Arg};
 use config::Config;
 use failure::Error;
-use std::{
-    collections::HashMap,
-    io::{self, Write},
-    path::PathBuf,
-    process::Command,
-};
+use std::{collections::HashMap, env, io::{self, Write}, path::PathBuf, process::Command};
 mod applyerr;
 use applyerr::ApplyError;
-mod action;
-use action::Action;
-
-
-fn arguments<'a>() -> clap::ArgMatches<'a> {
-    
-    let action_apply = "apply";
-    let action_unapply = "unapply";
-    let action_show = "show";
-    let action_is_applied = "is_applied";
-
-    //let actions = vec![action_show, action_apply, action_unapply, action_is_applied];
-    let not_apply =vec![action_show, action_unapply, action_is_applied];
-    let not_unapply =vec![action_show, action_apply, action_is_applied];
-    let not_show =vec![ action_apply, action_unapply, action_is_applied];
-    let not_is_applied =vec![action_show, action_apply, action_unapply];
-    
-
-    return App::new(crate_name!())
-        .version(crate_version!())
-        .author(crate_authors!())
-        .help(crate_description!())
-        .arg(
-            Arg::with_name("name")
-                .short("n")
-                .long("name")
-                .value_name("NAME")
-                .required(true)
-                .help("name")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name(action_unapply)
-                .short("u")
-                .long(action_unapply)
-                .conflicts_with_all(&not_unapply)
-                .help("call unapply script"),
-        )
-        .arg(
-            Arg::with_name(action_show)
-                .help("call unapply script")
-                .long(action_show)
-                .conflicts_with_all(&not_show)
-                .help("show parameters")
-        )
-        .arg(
-            Arg::with_name(action_apply)
-                .short("a")
-                .long(action_apply)
-                .conflicts_with_all(&not_apply)
-                .help("call apply if not applied script")
-        )
-        .arg(
-            Arg::with_name(action_is_applied)
-                .short("i")
-                .long("is")
-                .conflicts_with_all(&not_is_applied)
-                .help("return 0 if already applied")
-
-        )
-        .arg(
-            Arg::with_name("v")
-                .short("v")
-                .multiple(true)
-                .takes_value(true)
-                .help("verbosity"),
-        )
-        .arg(
-            Arg::with_name("debug")
-                .short("d")
-                .help("print debug information"),
-        )
-        .get_matches();
-}
+///mod action;
 
 fn load_config(c1: &mut Config) -> Result<&mut Config, config::ConfigError> {
     let c2 = c1.merge(config::Environment::with_prefix("APPLY"))?;
@@ -105,74 +25,107 @@ fn load_config(c1: &mut Config) -> Result<&mut Config, config::ConfigError> {
 fn main1() -> Result<(), Error> {
     dotenv::dotenv().ok();
     env_logger::init();
+    let args: Vec<String> = env::args().collect();
 
-    let matches = arguments();
+    let apply_command = seahorse::Command::new("apply")
+    .description("apply  [name] if not applied")
+    .alias("a")
+    .usage("apply(a) [name...]");
 
-    let c1 = &mut config::Config::default();
-    let conf = load_config(c1)?;
+    println!("cmd {:?}", apply_command.name);
+  
+    let app = seahorse::App::new(env!("CARGO_PKG_NAME"))
+    .description(env!("CARGO_PKG_DESCRIPTION"))
+    .author(env!("CARGO_PKG_AUTHORS"))
+    .version(env!("CARGO_PKG_VERSION"))
+    .usage("applied action name")
+    .action(apply_action)
+    .command(apply_command);
 
+    app.run(args);
+    
+  
+    Ok(())
+}
+fn help_action() {
+
+}
+fn get_script_directory(conf: &mut Config) -> PathBuf {
     let script_dir = match conf.get_str("script_dir") {
         Ok(val) => PathBuf::from(val),
         Err(_) => dirs::home_dir().unwrap_or(std::env::current_dir().unwrap()),
     };
     println!("script dir: {:?}", script_dir);
     let script_path = PathBuf::from(script_dir);
+    if !script_path.exists() {
+        panic!("{:?} does not exist", script_path);
+    }
+    script_path
+}
+#[test]
+fn test_appply() -> Result<(), Error> {
+    let c1 = &mut config::Config::default();
+    let conf = load_config(c1)?;
+    let name = "example1";   
+    do_is_applied(conf, name)?;    
+    do_apply(conf, name)?;    
+    Ok(())    
+}
+fn apply_action(c: &seahorse::Context) {
+    println!("apply_action");
+    let name: &str = c.args.first().unwrap();
+    debug!("apply_action {}", name);
+    let c1 = &mut config::Config::default();
+    let conf = load_config(c1).unwrap();
+    do_is_applied(conf, name).unwrap();    
+    do_apply(conf, name).unwrap();    
 
-    let action = if matches.is_present("apply") {
-        Action::Apply
-    } else if matches.is_present("is_applied") {
-        Action::IsApplied
-    } else if matches.is_present("unapply") {
-        Action::UnApply
-    } else if matches.is_present("show") {
-        Action::Show
-    } else {
-        Action::Usage
-    };
-    if action == Action::Usage {
-        matches.usage();
-        error!("usage");
+}
+fn find_scriptlet(conf: &mut Config, name: &str, action: &str) -> PathBuf {
+    let filename = format!("{}-{}", name, action);
+    debug!("script filename {}", filename);
+    let dir = get_script_directory(conf);
+    debug!("dir {:?}", dir);
+    let path = dir.join(filename);
+    trace!("script{:?}", path);
+    if !path.exists() {
+        println!("create file {:?}", path);
     }
-    let name = matches.value_of("name").unwrap();
-    debug!("name {:#?}", name);
-    //let ac = read_or_create_config("apply.toml");
+    println!("apply script {:?}", path);
+    path
+}
+fn do_apply(conf: &mut Config, name: &str) -> Result<(), Error> {
+    let action = "apply";
 
-    if action == Action::IsApplied {
-        let maybe_name_config = conf.get_table(name)?;
-        debug!("maybe_name_config {:#?}", maybe_name_config);
-        let script = format!("{}-{}.sh", name, "is-applied");
-        let path = script_path.join(script);
-        trace!("script{:?}", path);
-        if !path.exists() {
-            println!("create file {:?}", path);
-        }
-        println!("apply script {:?}", path);
-        let mut name_config: HashMap<String, String> = HashMap::new();
-        for (k, v) in maybe_name_config {
-            name_config.insert(k, v.into_str().unwrap());
-        }
-        debug!("params {:#?}", name_config);
-        is_applied(name, &path, name_config);
+    let script_path = find_scriptlet(conf, name, action);
+    
+    let maybe_name_config = conf.get_table(name)?;
+    debug!("maybe_name_config {:#?}", maybe_name_config);
+    let mut name_config: HashMap<String, String> = HashMap::new();
+    for (k, v) in maybe_name_config {
+        name_config.insert(k, v.into_str().unwrap());
     }
-    if action == Action::Apply {
-        let maybe_name_config = conf.get_table(name)?;
-        debug!("maybe_name_config {:#?}", maybe_name_config);
-        let script = format!("{}-{}.sh", name, "apply");
-        let path = script_path.join(script);
-        trace!("script{:?}", path);
-        if !path.exists() {
-            println!("create file {:?}", path);
-        }
-        println!("apply script {:?}", path);
-        let mut name_config: HashMap<String, String> = HashMap::new();
-        for (k, v) in maybe_name_config {
-            name_config.insert(k, v.into_str().unwrap());
-        }
-        debug!("params {:#?}", name_config);
-        is_applied(name, &path, name_config);
-    }
+    debug!("params {:#?}", name_config);
+    execute_apply(name, &script_path, name_config);
     Ok(())
 }
+
+fn do_is_applied(conf: &mut Config, name: &str) -> Result<(), Error> {
+    let action = "is-applied";
+
+    let script_path = find_scriptlet(conf, name, action);
+
+    let maybe_name_config = conf.get_table(name)?;
+    debug!("maybe_name_config {:#?}", maybe_name_config);
+    let mut name_config: HashMap<String, String> = HashMap::new();
+    for (k, v) in maybe_name_config {
+        name_config.insert(k, v.into_str().unwrap());
+    }
+    debug!("params {:#?}", name_config);
+    is_applied(name, &script_path, name_config);
+    Ok(())
+}
+
 
 fn execute_script(cmd: &PathBuf, vars: HashMap<String, String>) -> Result<(), ApplyError> {
     let cmdstr = cmd.as_os_str();
@@ -205,7 +158,18 @@ fn execute_script(cmd: &PathBuf, vars: HashMap<String, String>) -> Result<(), Ap
         None => Err(ApplyError::CmdExitedPrematurely),
     }
 }
-
+fn execute_apply(_name: &str, script: &PathBuf, vars: HashMap<String, String>) -> bool {
+    match execute_script(script, vars) {
+        Ok(_) => {
+            println!("{}", Green.paint("Applied"));
+            true
+        }
+        Err(_e) => {
+            println!("{}", Yellow.paint("Apply Failed"));
+            false
+        }
+    }
+}
 fn is_applied(_name: &str, script: &PathBuf, vars: HashMap<String, String>) -> bool {
     match execute_script(script, vars) {
         Ok(_) => {
